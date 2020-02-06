6b08c8ebe980da33fc7ca4b7d8bc5202b0f57548
Kelynn <Kelynn.c19@gmail.com>
2020-02-05 23:38:44 -0800

    Added movie descriptions and collection view

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Kelynn Ma on 1/30/20.
+//  Copyright © 2020 kelynn23. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
